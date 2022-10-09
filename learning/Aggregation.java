3ab93bcd72cbaf9df1fc68a0388a4964345fe80d
waynemorphic <wayneklyde@gmail.com>
2022-10-09 23:43:49 +0300

    aggregation in java

--- learning/Aggregation.java
+++ learning/Aggregation.java
@@ -1,6 +1,6 @@
 /*
  * - aggregation is when a class has entity reference
- * - entity reference is when an object A contains a reference to another object B. This means that object A has a 
+ * - entity reference is when an object A contains a reference to another object B. This means that object A has a
  *   HAS-A relationship with object B 
  */
 
@@ -9,5 +9,26 @@ package learning;
 public class Aggregation{
     public static void main(String[] args){
 
+        Circle circle1 = new Circle();
+        double resArea = circle1.circleArea(7);
+        System.out.print("The area of the circle is " + (resArea));
+    }
+}
+
+class MathOperation{
+    int square(int n){ // aggregation
+        return n * n;
+    }
+}
+
+class Circle{
+    MathOperation operationSquare;
+    final double PI = 3.14312;
+
+    // code reusabilty
+    double circleArea(int radius){
+        operationSquare = new MathOperation();
+        int radiusSquare = operationSquare.square(radius);
+        return PI * radiusSquare;
     }
 }
