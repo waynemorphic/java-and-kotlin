4ce142dd2bfc109979aaf77823e6443f6990c6e6
waynemorphic <wayneklyde@gmail.com>
2023-01-17 17:51:25 +0300

    comparing strings functio one

--- programs/CompareStrings.java
+++ programs/CompareStrings.java
@@ -1,44 +1,47 @@
 package programs;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.InputMismatchException;
+import java.util.List;
 import java.util.Scanner;
 
 public class CompareStrings {
-
-    public static String getSmallestAndLargest(String s, int k) {
+    // Complete the function
+    // 'smallest' must be the lexicographically smallest substring of length 'k'
+    // 'largest' must be the lexicographically largest substring of length 'k'
+    
+    public static String getSmallestAndLargest(String s, int k){
         String smallest = "";
         String largest = "";        
-        String entry = s.replace(" ", "");
-        int len = entry.length();
-        
-        // Complete the function
-        // 'smallest' must be the lexicographically smallest substring of length 'k'
-        // 'largest' must be the lexicographically largest substring of length 'k'
+        int len = s.length();
+        List<String> strs = new ArrayList<>();
 
         for (int i = 0; i < len; i++){
-            String subA = entry.substring(0, k);
-            String subB = entry.substring(i, k);
-            System.out.println(subA);
-            System.out.println(subB);
-
-            if (subA.compareTo(subB) < subB.compareTo(subA)){
-                smallest = subA;
-                largest = subB;
+            for (int j = i + k; j < len; j++){
+                String out = s.substring(i, j);
+                if (out.length() == 3){
+                    strs.add(out);
+                }
             }
-            else{
-                smallest = subB;
-                largest = subA;
-            }            
+
+            Collections.sort(strs);  
+            smallest = strs.get(0);
+            largest = strs.get(strs.size() - 1);
         }
         // System.out.println( smallest + "\n" + largest);
         return smallest + "\n" + largest;
     }
 
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        String s = scan.next();
-        int k = scan.nextInt();
-        scan.close();
+        try(Scanner scan = new Scanner(System.in)) {
+            String s = scan.next();
+            int k = scan.nextInt();
       
-        System.out.println(getSmallestAndLargest(s, k));
+            System.out.println(getSmallestAndLargest(s, k));
+        } catch (InputMismatchException e) {
+            System.out.println("Input should not have spaces: " + e);
+        }
+        
     }
 }
