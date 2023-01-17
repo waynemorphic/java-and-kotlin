dfbd7f4f51d2a4d198ffac156ebb121aa1bee412
waynemorphic <wayneklyde@gmail.com>
2023-01-17 17:59:42 +0300

    method 2 for comparing strings lexicographically

--- programs/CompareStrings.java
+++ programs/CompareStrings.java
@@ -10,7 +10,7 @@ public class CompareStrings {
     // Complete the function
     // 'smallest' must be the lexicographically smallest substring of length 'k'
     // 'largest' must be the lexicographically largest substring of length 'k'
-    
+
     public static String getSmallestAndLargest(String s, int k){
         String smallest = "";
         String largest = "";        
@@ -33,12 +33,31 @@ public class CompareStrings {
         return smallest + "\n" + largest;
     }
 
+    public static String getComparator(String s, int k){
+        String smallest = s.substring(0, k);
+        String largest = s.substring(0, k);
+        int len = s.length();
+
+        for (int i = 1; i < len - k; i++){
+            String out = s.substring(i, i + k);
+            if (out.compareTo(smallest) < 0){
+                smallest = out;
+            }
+            else if (out.compareTo(largest) > 0){
+                largest = out;
+            }
+        }
+        return smallest + "\n" + largest;
+    }
+
     public static void main(String[] args) {
         try(Scanner scan = new Scanner(System.in)) {
             String s = scan.next();
             int k = scan.nextInt();
       
-            System.out.println(getSmallestAndLargest(s, k));
+            System.out.println("Method one: " + getSmallestAndLargest(s, k));
+            System.out.println("Method 2: " + getComparator(s, k));
+            
         } catch (InputMismatchException e) {
             System.out.println("Input should not have spaces: " + e);
         }
