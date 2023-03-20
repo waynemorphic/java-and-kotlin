b75a68fa1a91f79b904bed18999a828fad34131d
waynemorphic <wayneklyde@gmail.com>
2023-03-20 08:43:22 +0300

    small change

--- programs/PageFlip.java
+++ programs/PageFlip.java
@@ -1,9 +1,9 @@
 package programs;
 
 /*
- * A teacher asks the class to open their books to a page number. A student can either start turning pages from the 
- * front of the book or from the back of the book. They always turn pages one at a time. When they open the book, page 1
-   is always on the right side:
+A teacher asks the class to open their books to a page number. A student can either start turning pages from the 
+front of the book or from the back of the book. They always turn pages one at a time. When they open the book, page 1
+is always on the right side:
 
 When they flip page 1, they see pages 2 and 3. Each page except the last page will always be printed on both sides. 
 The last page may only be printed on the front, given the length of the book. If the book is n pages long, and a student wants to turn to page p
